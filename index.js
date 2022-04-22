97fd8f05f92228c8c56749698b6c919824ae4917
haroon11 <haroon@netart.io>
2022-04-22 16:51:39 +0530

    updated from http to https

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 require('dotenv').config()
 
-const http = require('http')
+const https = require('https')
 const app = require('./app')
-const path = require('path')
 const fs = require('fs')
+
+const privateKey = fs.readFileSync(
+  '/etc/letsencrypt/live/dap.digitalaidedschool.com/privkey.pem',
+  'utf8'
+)
+const certificate = fs.readFileSync(
+  '/etc/letsencrypt/live/dap.digitalaidedschool.com/cert.pem',
+  'utf8'
+)
+const ca = fs.readFileSync(
+  '/etc/letsencrypt/live/dap.digitalaidedschool.com/chain.pem',
+  'utf8'
+)
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+}
 
 const portNumber = process.env.PORT_NUMBER || 8000
 
-http
-  .createServer(
-    app
-  )
+https
+  .createServer(credentials, app)
   .listen(process.env.PORT || portNumber, () => {
     console.log(`running on ${process.env.PORT || portNumber}`)
   })
