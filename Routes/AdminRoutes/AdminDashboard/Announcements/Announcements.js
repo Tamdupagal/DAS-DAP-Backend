bd61d82c2db95cd56dde4e1aebe6d5abca7000ca
raghav <raghavsharma6996@gmail.com>
2022-09-27 15:39:12 +0530

    announcement changes

--- Routes/AdminRoutes/AdminDashboard/Announcements/Announcements.js
+++ Routes/AdminRoutes/AdminDashboard/Announcements/Announcements.js
@@ -16,7 +16,6 @@ Router.post("/", [announcementSchemaValidation, createAnnouncement]);
 Router.route("/search?").get(viewAnnouncements);
 Router.route("/view?").get(viewSelectedAnnouncement);
 
-
 Router.route("/viewAll?").get(viewAllAnnouncement)
 Router.route("/searchResponses?").get(viewAnnouncementResponse);
 module.exports = Router;
