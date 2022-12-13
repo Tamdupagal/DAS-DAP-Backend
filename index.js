215c1ddd523055781838dd9c9fc0505eac7bd9ed
Tamdupagal <83121504+Tamdupagal@users.noreply.github.com>
2022-12-13 12:26:06 +0530

    Raghav backend (#42)
    * Re-deploy
    
    * fix some errors
    
    * adding superadmin authorization
    
    * adding superadmin routes & changes in analytics
    
    * announcement and analytics chaanges
    
    * changes in announcement & feedback model
    
    * solving userfeedback bug
    
    * adding delete announcement & taskflow pagination
    
    * adding socket.io to backend
    
    Co-authored-by: raghav <raghavsharma6996@gmail.com>
    Co-authored-by: shobhitchoudhary745 <shobhitchoudhary745@gmail.com>

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-require('dotenv').config()
-const portNumber = process.env.PORT_NUMBER || 8001
-const cluster = require('cluster')
-const totalCPUs = require('os').cpus()
-const logger = require('./Services/Logger/Logger')
-//const https = require('https')
-const app = require('./app')
-const fs = require('fs')
-const http = require('http')
-
+require("dotenv").config();
+const portNumber = process.env.PORT_NUMBER || 8001;
+const cluster = require("cluster");
+const totalCPUs = require("os").cpus();
+const logger = require("./Services/Logger/Logger");
+const app = require("./app");
+const fs = require("fs");
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: { origin: "http://localhost:3000" },
+});
 // const privateKey = fs.readFileSync(process.env.privateKey, 'utf8')
 // const certificate = fs.readFileSync(process.env.certificate, 'utf8')
 // const ca = fs.readFileSync(process.env.ca, 'utf8')
+io.on("connection", (socket) => {
+  socket.on("join", (options, callback) => {
+    console.log('join')
+    socket.join(options.senderId); 
+  });
+  socket.on("message", (options, callback) => {
+    if (io.sockets.adapter.rooms.get(options.receiverId) === undefined) {
+      console.log("offline");   
+    }
+    console.log('online')
+    io.to(options.receiverId).emit("message", { message: options.message });
+  });
+});
 
 // const credentials = {
 //   key: privateKey,
@@ -20,23 +35,25 @@ const http = require('http')
 
 if (cluster.isMaster) {
   totalCPUs.forEach(async (node) => {
-    await cluster.fork()
-  })
+    await cluster.fork();
+  });
 
-  cluster.on('exit', async (worker, code, signal) => {
-    logger.info(`Worker ${worker.process.pid} has died!`)
-    logger.error(`Worker ${worker.process.pid} has died!`)
-    logger.info('Creating a new Worker')
-    await cluster.fork()
-  })
+  cluster.on("exit", async (worker, code, signal) => {
+    logger.info(`Worker ${worker.process.pid} has died!`);
+    logger.error(`Worker ${worker.process.pid} has died!`);
+    logger.info("Creating a new Worker");
+    await cluster.fork();
+  });
 } else {
   // https
   //   .createServer(credentials, app)
   //   .listen(process.env.PORT || portNumber, () => {
   //     console.log(`running on ${process.env.PORT || portNumber}`)
 
-    //  })
-  http.createServer(app).listen(process.env.PORT || portNumber, () => {
-    logger.info(`Process ${process.pid} is online on port number ${portNumber}`)
-  }) 
+  //  })
+  server.listen(process.env.PORT || portNumber, () => {
+    logger.info(
+      `Process ${process.pid} is online on port number ${portNumber}`
+    );
+  });
 }
