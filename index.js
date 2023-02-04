0633767fa8eb8de77796eeb4a82eec4ca6b5c745
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2023-02-04 14:52:59 +0530

    minor changes in socket.io

--- index.js
+++ index.js
@@ -47,6 +47,9 @@ io.on("connection", (socket) => {
      }
   })
   
+  socket.on("createAnnouncement",(options,callback)=>{
+    io.to(options.companyName).emit("announcementCreated",options);
+  })
   socket.on("joinGroup", (options, callback) => {
     console.log('user join ',options.roomName)
     socket.join(options.roomName);
@@ -58,6 +61,9 @@ io.on("connection", (socket) => {
     io.to(options.groupName).emit("getLatestNotification", options);
   });
 
+  socket.on("joinCompany",(options,callback)=>{
+    socket.join(options.companyName)
+  })
   socket.on("deleteMsg",(options,callback)=>{
     console.log("deleted msg start")
     io.to(options.receiverId).emit("deletedMsg",options)
@@ -66,6 +72,10 @@ io.on("connection", (socket) => {
   socket.on("deleteGroupMsg",(options,callback)=>{
     console.log("deleted group msg start")
     socket.broadcast.to(options.groupName).emit("groupMsgDelete",options)
+  })
+
+  socket.on("joinAdminRoom",(options,callback)=>{
+    socket.join("adminRoom");
   })
 
   socket.on('startTyping',(options,callback)=>{
