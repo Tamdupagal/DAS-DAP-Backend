e29b3da749e861e90fa5a5387cdcfe357f24dfa0
raghav <raghavsharma6996@gmail.com>
2023-02-16 16:01:27 +0530

    changes in validator

--- Validators/UserCreationValidation.js
+++ Validators/UserCreationValidation.js
@@ -1,44 +1,45 @@
-const ajv = new (require('ajv'))()
+const ajv = new (require("ajv"))();
 
 const schema = {
-  type: 'object',
+  type: "object",
   properties: {
-    userName: { type: 'string' },
+    userName: { type: "string" },
     email: {
-      type: 'string',
-      pattern: '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$',
+      type: "string",
+      pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$",
     },
 
-    companyEmail:{ type: 'string' },
-    password: { type: 'string' },
+    companyEmail: { type: "string" },
+    typeOfUser: { type: "string" },
+    password: { type: "string" },
   },
-  required: ['userName', 'email', 'password','companyEmail'],
+  required: ["userName", "email", "password", "companyEmail", "typeOfUser"],
   additionalProperties: false,
-}
+};
 
 module.exports = {
   async userCreationValidation(req, res, next) {
     try {
-      const validate = ajv.compile(schema)
-      let validity = await validate(req.body)
+      const validate = ajv.compile(schema);
+      let validity = await validate(req.body);
       if (!validity) {
         res.status(400).send({
           status: 400,
           message:
-            validate.errors[0].instancePath.split('/').slice(-1) +
-            ' ' +
+            validate.errors[0].instancePath.split("/").slice(-1) +
+            " " +
             validate.errors[0].message,
           data: validate.errors,
-        })
+        });
       } else {
-        next()
+        next();
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
       res.status(400).send({
         status: 500,
-        message: 'Internal Server Error- from User-Creation Validation',
-      })
+        message: "Internal Server Error- from User-Creation Validation",
+      });
     }
   },
-}
+};
